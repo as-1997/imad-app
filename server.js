f53f19f4f6d0e83a95320c04a26ae43ebf2f329e
Arunima Setua <31192604+as-1997@users.noreply.github.com>
2017-08-28 16:11:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req,res){
-    res.send('Article-one is being requested so here it comes');
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-two', function (req,res){
-    res.send('Article-two is being requested so here it comes');
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
