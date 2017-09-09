6dbe4bdbd1903a835dd7d928b740dbd01147fa9e
Arunima Setua <31192604+as-1997@users.noreply.github.com>
2017-09-09 11:40:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,17 @@ console.log('Loaded!');
 var element=document.getElemntById('main-text');
 
 element.innerHTML='New value';
+
+var img=document.getElementById('madi');
+
+
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+}
